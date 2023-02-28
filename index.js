1770ffb43def0b23ce4764e76be7f0ba7fd8ddb4
kartikey <kartikeysood@rediffmail.com>
2023-03-01 01:41:03 +0530

    dynamic port

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ const userRoute = require("./src/route/user.route");
 const eventRoute = require("./src/route/event.route");
 app.use("/user", userRoute);
 app.use("/event", eventRoute);
-app.listen("5000", () => {
+app.listen(process.env.port, () => {
   console.log("listening on 5000");
 });
